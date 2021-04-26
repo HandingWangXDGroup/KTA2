47a824dc9b1fc41aef157e3b82a2503e027a3f71
zssongzs <74448034+zssongzs@users.noreply.github.com>
2021-04-26 16:46:02 +0800

    Update K_UpdateCA.m

--- K_UpdateCA.m
+++ K_UpdateCA.m
@@ -2,14 +2,7 @@ function [CAobj,CAdec,CAvar] = K_UpdateCA(CAobj,CAdec,CAvar,MaxSize)
 % [CAobj,CAdec] = UpdateCA_GP(CAobj,CAdec,OffspringDec,OffspringObj,Maxsize);
 % Update CA
 
-%--------------------------------------------------------------------------
-% Copyright (c) 2016-2017 BIMK Group. You are free to use the PlatEMO for
-% research purposes. All publications which use this platform or any code
-% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
-% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB Platform
-% for Evolutionary Multi-Objective Optimization [Educational Forum], IEEE
-% Computational Intelligence Magazine, 2017, 12(4): 73-87".
-%--------------------------------------------------------------------------
+
 
     N  = size(CAobj,1);
     if N <= MaxSize
