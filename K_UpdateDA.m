94cb063ddc073d26b6a1b7ad71e414838f9e619f
zssongzs <74448034+zssongzs@users.noreply.github.com>
2021-04-26 16:47:39 +0800

    Update K_UpdateDA.m

--- K_UpdateDA.m
+++ K_UpdateDA.m
@@ -1,17 +1,5 @@
 function [DAobj,DAdec,DAvar] = K_UpdateDA(DAobj,DAdec,DAvar,MaxSize,p)
-% [DAobj,DAdec] = UpdateDA_GP(DAobj,DAdec,OffspringDec,OffspringObj,Global.N,p);
 % Update DA
-
-%--------------------------------------------------------------------------
-% Copyright (c) 2016-2017 BIMK Group. You are free to use the PlatEMO for
-% research purposes. All publications which use this platform or any code
-% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
-% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB Platform
-% for Evolutionary Multi-Objective Optimization [Educational Forum], IEEE
-% Computational Intelligence Magazine, 2017, 12(4): 73-87".
-%--------------------------------------------------------------------------
-
-
 
     DA_Nor_pre = (DAobj - repmat(min(DAobj,[],1),size(DAobj,1),1))...
 ./repmat(max(DAobj,[],1) - min(DAobj,[],1),size(DAobj,1),1);
